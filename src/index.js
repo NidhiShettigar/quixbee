ce8791be7530a3f9a4acfdea8e66428f8f347d2a
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-10 20:07:55 +0530

    layout created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "./assets/style.css";
+
+class QuizBee extends Component {
+  render() {
+    return(
+      <div className='container'>
+        <div className='title'>QuizBee</div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <QuizBee />,
+  document.getElementById('root')
+);
+
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,4 +33,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
